03e7427fbc6515e506f69f16a8248517ddfecf76
Nicholaos Bilalis <n.bilalis@sae.edu>
2021-11-20 01:00:46 +0200

    Add support for scoped css

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import reactScopedCssPlugin from 'rollup-plugin-react-scoped-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactScopedCssPlugin() as any],
   base: '',
 });
