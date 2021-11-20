177b8d2b7e6c262621d5cdf7194754dd4029e0e1
Nicholaos Bilalis <n.bilalis@sae.edu>
2021-11-20 20:30:57 +0200

    Add support for absolute paths in imports
    - aleclarson/vite-tsconfig-paths: Support for TypeScript's path mapping in Vite - https://bit.ly/30NttXs
    - vue.js - Vue 3 on Vite.js with Eslint — Unable to resolve path to module eslint(import/no-unresolved) - Stack Overflow - https://bit.ly/329Q5Sa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+
 import react from '@vitejs/plugin-react';
 import reactScopedCssPlugin from 'rollup-plugin-react-scoped-css';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactScopedCssPlugin() as any],
+  plugins: [react(), tsconfigPaths(), reactScopedCssPlugin() as any],
   base: '',
 });
